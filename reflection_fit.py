8259b14316170846169b91992eebdb4be4f54bba
s-keesing <78819719+s-keesing@users.noreply.github.com>
2024-05-24 13:17:20 -0700

    Update reflection_fit.py

--- reflection_fit.py
+++ reflection_fit.py
@@ -3,20 +3,12 @@ from scipy.optimize import curve_fit
 from Lorentz_function import dip_func
 from numpy import genfromtxt
 
-my_data = genfromtxt('na_scan_send.csv', delimiter=',')
-f = my_data[:,1]
-p = my_data[:,2]
-
-pt, pc = reflection_fit(f,p)
-print(pt)
-print(pc)
-
 def reflection_fit(freq, real_amp):
 
     # turn arrays into numpy arrays
     freq = np.asarray(freq)
     real_amp = np.asarray(real_amp)
-    im_amp = np.asarray(im_amp)
+    #im_amp = np.asarray(im_amp)
 
     # use the real and imaginary components of power to find gamma squared
     #gamma_sq = np.add(np.square(real_amp), np.square(im_amp))
@@ -27,7 +19,7 @@ def reflection_fit(freq, real_amp):
     f0 = freq[f0_arg]
 
     # define a guess for C
-    gamma_arg = np.round((gamma_sq.size)/3)
+    gamma_arg = int(np.round((gamma_sq.size)/3))
     gamma_C = gamma_sq[-gamma_arg:]
     C = np.median(gamma_C)
 
@@ -36,6 +28,7 @@ def reflection_fit(freq, real_amp):
 
     # find bandwidth
     gamma_sq_Q1 = np.abs(gamma_sq[f0_arg:] - (C - delta_y/2))
+    gamma_sq[-f0_arg:]
     df1 = freq[np.argmin(gamma_sq_Q1)]
     gamma_sq_Q2 = np.abs(gamma_sq[-f0_arg:] - (C - delta_y/2))
     df2 = freq[np.argmin(gamma_sq_Q2)]
@@ -48,3 +41,9 @@ def reflection_fit(freq, real_amp):
     params = [delta_y, Q, f0, C]
     popt, pcov = curve_fit(dip_func, freq, gamma_sq, params)
     return popt, pcov
+
+my_data = genfromtxt(r'C:\Users\senna\OneDrive\Documents\UW\ADMX\na_scan_send.csv', delimiter=',')
+f = my_data[:,0]
+p = my_data[:,1]
+
+pt, pc = reflection_fit(f,p)
