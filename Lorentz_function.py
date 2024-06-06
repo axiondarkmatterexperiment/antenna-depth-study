d438561d19464e4032d3f84102155f021bfd5400
s-keesing <78819719+s-keesing@users.noreply.github.com>
2024-06-06 15:13:47 -0700

    Update Lorentz_function.py

--- Lorentz_function.py
+++ Lorentz_function.py
@@ -4,3 +4,7 @@ import numpy as np
 def dip_func(f, dy, Q, f0, C):
     delta = Q*(f - f0)/f0
     return C - dy/(1 + 4*np.square(delta))
+
+def peak_func(f, dy, Q, f0, C):
+    delta = Q*(f - f0)/f0
+    return C + dy/(1 + 4*np.square(delta))
